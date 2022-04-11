71d307a3eb9cf444111614ef367ba606c02e4ba9
LemanMammadly <lamanam@code.edu.az>
2022-04-11 23:59:34 +0400

    homework

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            do
+            {
+                Console.WriteLine("Username daxil edin :");
+                string name = Console.ReadLine();
+                Console.WriteLine("Passwordu daxil edin :");
+                string passw = Console.ReadLine();
+                User user = new User(name, passw);
+                user.Username = name;
+
+
+
+                int countUpper = 0;
+                int countLower = 0;
+                int countDigit = 0;
+                foreach (char item in passw)
+                {
+                    if (char.IsUpper(item))
+                    {
+                        countUpper++;
+                    }
+                    else if (char.IsLower(item))
+                    {
+                        countLower++;
+                    }
+                    else if (char.IsDigit(item))
+                    {
+                        countDigit++;
+                    }
+                }
+                if (countUpper >= 1 && countLower >= 1 && countDigit >= 1)
+                {
+                    user.PassWord = passw;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Yanlis parol..");
+                }
+
+            } while (true);
+
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class User
+    {
+        private string _userName;
+        private string _passWord;
+
+
+       
+        public string Username
+        {
+            get 
+            { 
+                return _userName;
+            }
+            set
+            {
+                if (value.Length >=8 )
+                {
+                    _userName = value;
+                }
+                else
+                {
+                    Console.WriteLine("Minimum 8 uzunlugunda olamlidir..");
+                }
+            }
+        }
+
+        public string PassWord
+        {
+            get
+            {
+                return _passWord;
+            }
+            set
+            {
+                if (value.Length >= 8)
+                {
+                    _passWord = value;
+                }
+                else
+                {
+                    Console.WriteLine("Minimum 8 uzunlugunda olmalidir..");
+                }
+            }
+        }
+
+        public User(string username,string password)
+        {
+            Username = username;
+            PassWord = password;
+        }
+
+    }
+
+
+}
